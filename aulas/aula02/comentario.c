e3c5742752fadee5c02437ae544a21aafd13b547
Sameki__ <samekichigameruno@gmail.com>
2026-03-11 21:57:31 -0300

    Adiciona codigos da Aula02

---
+++ aulas/aula02/comentario.c
@@ -0,0 +1,18 @@
+/*
+
+Este programa explica como usar
+Comentarios na Linguagem C
+
+ */
+
+#include <stdio.h>
+// função princiapl que é chamada pelo Windows
+
+int main(){
+    /* Etapas na Criação de um codigo:
+        1- Entrada
+        2- Processamento
+        3- Saida
+    */
+    return 0;
+}
---
+++ aulas/aula02/saida.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("estou usando a funcao printf()\n");
+
+    printf("Programar em C eh TOP!\n");
+
+    printf("%011i\n", 10+10);
+
+    printf("%f\n", 3.1415);
+    printf("%.4f\n", 3.1415);
+
+    printf("%s", "Isso eh uma string\n");
+
+    return 0;
+}
