18ce579af83868dc3ef2b5d0972d2e239ae66441
Sameki__ <samekichigameruno@gmail.com>
2026-04-01 20:06:18 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    
+
+    printf("%c\n", 'A');
+    printf("\n ");
+    printf("%s", "Isso eh uma String\n");
+
+    return 0;
+
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("%d\n %d\n %05i\n", 10, -5, 00004);
+
+    return 0;
+}
